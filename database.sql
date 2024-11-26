239647ddada298317800efade78fb4f658201d7a
sonnh1712 <sonnh1712@gmail.com>
2024-11-26 16:07:40 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -2,7 +2,18 @@ create database qlnv;
 use qlnv;
 create table roles(
 	id int primary key auto_increment,
-	name varchar(15) not null,
+	name varchar(15) not null unique,
 	des varchar(30) not null,
 	check (name != '' and des != '')
+);
+create table users(
+	id int primary key auto_increment,
+	name varchar(30) not null,
+	email varchar(30) not null unique,
+	password varchar(60) not null,
+	address varchar(30) not null,
+	phone varchar(10) not null,
+	role_id int not null,
+	foreign key (role_id) references roles(id),
+	check (name != '' and email != '' and password != '' and address != '' and char_length(phone) = 10 and role_id != '')
 );
